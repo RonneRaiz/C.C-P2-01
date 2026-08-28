141a2a2f50326c8125a70a5e61963136fe13a556
Ronne <ronneraizlima@gmail.com>
2026-08-28 02:47:54 -0300

    Delete 02_soma.py

--- 02_soma.py
+++
@@ -1,8 +0,0 @@
-print(" *Comentario* professora o inputs e os prints estão em inglês, mas é porque eu gosto mas eu juro que foi eu quem fiz :)")
-
-numero_a = float(input("enter your first number "))
-numero_b = float(input("enter your second number "))
-
-soma = numero_a + numero_b 
-
-print (f"sua soma é {soma} ")
